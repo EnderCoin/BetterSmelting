9a8b04e3b660a5f909fb6851914782cba80a86c8
SoniEx2 <endermoneymod@gmail.com>
2014-07-16 20:13:01 -0300

    Added @API

---
+++ src/main/java/com/github/soniex2/bettersmelting/api/package-info.java
@@ -0,0 +1,4 @@
+@API(owner = Reference.MOD_ID, apiVersion = "1.0", provides = Reference.MOD_ID + "API") package com.github.soniex2.bettersmelting.api;
+
+import com.github.soniex2.bettersmelting.reference.Reference;
+import cpw.mods.fml.common.API;
